6fe9357d89a604fb5d2c318633a1bb60b0399f23
Manab-Bairagi <manab04.official@gmail.com>
2025-01-17 23:57:37 +0530

    debugging

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -18,7 +18,7 @@ function AuthForm({ setIsAuthenticated, setShowAuth }) {
 
     try {
       const endpoint = isLogin ? '/api/login' : '/api/register';
-      const response = await fetch(`http://localhost:5000${endpoint}`, {
+      const response = await fetch(`https://movie-master-kgnu.onrender.com${endpoint}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
